00e4b0cb494936dfb915a27ec2913814e0c297a3
Alex Langenfeld <alangenfeld@wisc.edu>
2010-04-15 23:15:33 -0500

    added journal.c

---
+++ journal.c
@@ -0,0 +1,28 @@
+#include "types.h"
+#include "defs.h"
+#include "param.h"
+#include "stat.h"
+#include "mmu.h"
+#include "proc.h"
+#include "fs.h"
+#include "fsvar.h"
+#include "file.h"
+#include "fcntl.h"
+
+int j_init()
+{
+
+  return 0;
+}
+
+int j_iupdate()
+{
+
+  return 0;
+}
+
+int j_writei()
+{
+
+  return 0;
+}
